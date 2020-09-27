94c284249666ff4c196f3a8d7d85f68fbf50273d
Anthony <Arixamp23@gmail.com>
2020-09-26 23:04:45 -0430

    Se añadió la función conectar y crear alarma, ALPHA

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import time
 import tkinter as tk
 import sqlite3
-
+from tkinter import messagebox
+"""
 root=tk.Tk()
 root.title("Notir")
 root.config(bg="white")
@@ -11,11 +12,11 @@ frame.pack()
 
 pantalla_reloj=tk.Label(frame, text="", font=("ArcaMajora3-Bold", 30), bg="white", fg="gray")
 pantalla_reloj.grid(row="0", column="0")
-
 """
-Para almacenar las alarmas necesitaría utilizar una base de datos
-MongoDB será el lenguaje que empezaré a aprender ahora mismo para
-implementarlo en este proyecto, espero no tardar más de lo creo.
+"""
+Investigando un poco me dí cuenta que mongoDB no es
+una buena opción para este proyecto, y debido a que
+no necesito usuarios decidí usar SQlite3.
 
 """
 
@@ -25,12 +26,40 @@ def update_clock():
 	pantalla_reloj.configure(text=update)
 	frame.after(1000, update_clock)
 
-def conect_database():
+def connect_database():
 
 	conection= sqlite3.connect("notir_database")
 	cursor= conection.cursor()
 
-	#"En proceso"# cursor.execute("CREATE TABLE Alarma")
+	try:
+		cursor.execute("CREATE TABLE ALARMAS(NOMBRE VARCHAR(20), DESCRIPCION VARCHAR(70), AVISAR VARCHAR(5), FECHA VARCHAR(10), CREADA VARCHAR(8))")
+	
+	except sqlite3.OperationalError:
+		return "conectado a base de datos ya existente."
+	
+	cursor.close()
+	conection.close()
+	
+def create_alarm():
 
-update_clock()
-root.mainloop()
+	conection= sqlite3.connect("notir_database")
+	cursor= conection.cursor()
+
+	create_date= time.strftime("%H:%M:%S")
+	alarm_clock= "23:00"
+	alarm_date=	"2020-09-26"
+
+	tupla_values="Estudiar JavaScript", "Necesito hacerlo porque quiero hacer páginas web", alarm_clock, alarm_date, create_date
+	cursor.execute("INSERT INTO ALARMAS VALUES(?,?,?,?,?)",(tupla_values))
+	
+	messagebox.showinfo("Crear alarma", "¡Alarma creada!")
+
+	conection.commit()
+	cursor.close()
+	conection.close()
+
+
+#update_clock()
+connect_database()
+create_alarm()
+#root.mainloop()
